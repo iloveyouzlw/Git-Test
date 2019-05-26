f2708059a3af3728ab43497c67e27c25bad82f64
zhanglw <baiduzhanglingwei@qq.com>
2019-05-26 23:57:21 +0800

    #，，，，，，，

--- CITest/Controllers/ValuesController.cs
+++ CITest/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace CITest.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2", "value3", "value4" };
+            return new string[] { "value1", "value2", "value3", "value4","value7" };
         }
 
         // GET api/values/5
